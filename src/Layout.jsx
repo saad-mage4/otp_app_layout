73b30c964244a13b457a397be86be822bbcf3650
saad-mage4 <saad@mage4.com>
2024-05-17 01:09:00 +0500

    update

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,6 +2,13 @@ const Layout = ({ children }) => {
   return (
     <>
       <div className="container">
+        <div className="message-box">
+          <p>
+            You are on trial period, Activate the paid plan to not miss any COD
+            Verifications!
+          </p>
+          <a href="#!">Recharge Now</a>
+        </div>
         {children}
       </div>
     </>
--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,6 +1,38 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Tooltip } from "react-tooltip";
 const SideBar = () => {
+  const [active, setActive] = useState("");
+
+  const colorPalette = (e) => {
+    e.preventDefault();
+    setActive("active");
+    if (active == "active") {
+      setActive("");
+    }
+  };
+
+  const colorBlack = (e) => {
+    e.preventDefault();
+    document.documentElement.style.setProperty("--primary-color", "#000");
+    setActive("");
+  };
+  const colorRed = (e) => {
+    e.preventDefault();
+    document.documentElement.style.setProperty("--primary-color", "#c20000");
+    setActive("");
+  };
+  const colorGreen = (e) => {
+    e.preventDefault();
+    document.documentElement.style.setProperty("--primary-color", "#217c00");
+    setActive("");
+  };
+  const colorPurple = (e) => {
+    e.preventDefault();
+    document.documentElement.style.setProperty("--primary-color", "#6957e9");
+    setActive("");
+  };
+
   return (
     <>
       <div className="sidebar_wrapper">
@@ -57,6 +89,35 @@ const SideBar = () => {
             >
               <i className="fas fa-address-book"></i>
             </a>
+          </li>
+        </ul>
+        <ul className="tabs-links app-settings">
+          <li>
+            <a
+              href="#!"
+              id="color-palette"
+              className={`${active}`}
+              data-tooltip-id="my-tooltip"
+              data-tooltip-content="Color Palette"
+              data-tooltip-offset={32}
+              onClick={colorPalette}
+            >
+              <i className="fas fa-palette"></i>
+            </a>
+            <div className="colors-box">
+              <a href="#!" onClick={colorBlack}>
+                <i className="fas fa-circle black"></i>
+              </a>
+              <a href="#!" onClick={colorRed}>
+                <i className="fas fa-circle red"></i>
+              </a>
+              <a href="#!" onClick={colorGreen}>
+                <i className="fas fa-circle green"></i>
+              </a>
+              <a href="#!" onClick={colorPurple}>
+                <i className="fas fa-circle purple"></i>
+              </a>
+            </div>
           </li>
         </ul>
       </div>
